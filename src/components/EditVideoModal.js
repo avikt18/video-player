0436cd116aa7cd577c60e3e4e6355a4acc0bf5f0
avikt <avikumrawat18@gmail.com>
2023-03-08 18:50:25 +0530

    refactor main

--- src/components/EditVideoModal.js
+++ src/components/EditVideoModal.js
@@ -1,19 +1,13 @@
-import { useState } from "react";
-import { Modal, Form, Input, Select, message, Button } from "antd";
-import { useSelector, useDispatch } from "react-redux";
+import { Form, Input, message, Modal, Select } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import { editVideoThunk } from "../redux/videoSlice";
-import { createBucketThunk } from "../redux/bucketSlice";
-import { PlusOutlined } from "@ant-design/icons";
-import { useParams } from "react-router-dom";
 const { Option } = Select;
 
 function EditVideoModal({ isOpen, setIsOpen, video }) {
   const bucketList = useSelector((state) => state.buckets.bucketList);
   const loading = useSelector((state) => state.videos.loading);
-  const [newBucket, setNewBucket] = useState(false);
   const [form] = Form.useForm();
   const dispatch = useDispatch();
-  const { id } = useParams();
 
   const handleEdit = (values) => {
     console.log(values);
@@ -100,42 +94,6 @@ function EditVideoModal({ isOpen, setIsOpen, video }) {
             ))}
           </Select>
         </Form.Item>
-        {/* <Form.Item
-          name={newBucket ? "newBucketName" : "bucketId"}
-          label="Bucket Name"
-          rules={[
-            {
-              required: true,
-              message: "Please select bucket name",
-            },
-          ]}
-        >
-          {newBucket ? (
-            <Input placeholder="New Bucket" />
-          ) : (
-            <Select placeholder="Select a bucket">
-              {bucketList?.map((bucket, id) => (
-                <Option key={bucket.id || id} value={bucket?.id}>
-                  {bucket.name}
-                </Option>
-              ))}
-            </Select>
-          )}
-        </Form.Item>
-        {!newBucket ? (
-          <Button
-            type="dashed"
-            icon={<PlusOutlined />}
-            block
-            onClick={() => setNewBucket(true)}
-          >
-            New Bucket
-          </Button>
-        ) : (
-          <Button type="dashed" block onClick={() => setNewBucket(false)}>
-            Select bucket
-          </Button>
-        )} */}
       </Form>
     </Modal>
   );
